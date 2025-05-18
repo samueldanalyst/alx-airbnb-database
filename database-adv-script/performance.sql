216681194c0acb6cee0f115b4b4bee7ff9849a8b
samueldanalyst <samuelvictor747futa@gmail.com>
2025-05-18 14:40:22 +0100

    analyze

--- database-adv-script/performance.sql
+++ database-adv-script/performance.sql
@@ -1,22 +1,69 @@
--- Original Complex Query
--- Retrieves all bookings with user, profile, property, and payment details
+SELECT 
+    b.booking_id,
+    b.created_at AS booking_date,
+    b.startdate,
+    b.enddate,
+    b.status,
 
-SELECT
-    bookings.id AS booking_id,
-    users.id AS user_id,
-    users.email,
-    profiles.full_name,
-    properties.id AS property_id,
-    properties.title,
-    properties.location,
-    bookings.start_date,
-    bookings.end_date,
-    bookings.status,
-    payments.amount,
-    payments.payment_method,
-    payments.paid_at
-FROM bookings
-JOIN profiles ON bookings.booked_by = profiles.id
-JOIN users ON profiles.user_id = users.id
-JOIN properties ON bookings.property_id = properties.id
-LEFT JOIN payments ON payments.booking_id = bookings.id;
+    -- User details
+    u.user_id,
+    u.email,
+    u.account_status,
+
+    -- Profile details
+    p.first_name,
+    p.last_name,
+    p.phone_number,
+
+    -- Property details
+    pl.property_id,
+    pl.title AS property_title,
+    pl.description,
+    pl.rent,
+
+    -- Payment details
+    pay.payment_id,
+    pay.payment_amount,
+    pay.payment_status
+
+FROM bookings b
+JOIN profiles p ON b.profile_id = p.profile_id
+JOIN users u ON p.user_id = u.user_id
+JOIN properties pl ON b.property_id = pl.property_id
+JOIN payment pay ON b.payment_id = pay.payment_id;
+
+
+EXPLAIN ANALYZE
+SELECT 
+    b.booking_id,
+    b.created_at AS booking_date,
+    b.startdate,
+    b.enddate,
+    b.status,
+
+    -- User details
+    u.user_id,
+    u.email,
+    u.account_status,
+
+    -- Profile details
+    p.first_name,
+    p.last_name,
+    p.phone_number,
+
+    -- Property details
+    pl.property_id,
+    pl.title AS property_title,
+    pl.description,
+    pl.rent,
+
+    -- Payment details
+    pay.payment_id,
+    pay.payment_amount,
+    pay.payment_status
+
+FROM booking b
+JOIN profile_table p ON b.profile_id = p.profile_id
+JOIN user_table u ON p.user_id = u.user_id
+JOIN property_listing pl ON b.property_id = pl.property_id
+JOIN payment pay ON b.payment_id = pay.payment_id;
