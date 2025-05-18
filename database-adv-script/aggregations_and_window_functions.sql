13a44be08c1729e02432deca227d92d5b473f2d6
samueldanalyst <samuelvictor747futa@gmail.com>
2025-05-18 13:23:23 +0100

    push windows function

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -7,3 +7,16 @@ JOIN profiles ON users.id = profiles.user_id
 LEFT JOIN bookings ON profiles.id = bookings.booked_by
 GROUP BY users.id, users.email
 ORDER BY total_bookings DESC;
+
+
+SELECT 
+    p.id AS property_id,
+    p.title,
+    COUNT(b.id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
+FROM 
+    properties p
+LEFT JOIN 
+    bookings b ON p.id = b.property_id
+GROUP BY 
+    p.id, p.title;
