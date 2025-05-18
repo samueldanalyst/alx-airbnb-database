20eba7d358534f4d6b243f949c4331fe970dc2fc
samueldanalyst <samuelvictor747futa@gmail.com>
2025-05-18 13:20:31 +0100

    push sub query

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -8,3 +8,13 @@ WHERE id IN (
     GROUP BY properties.id
     HAVING AVG(reviews.rating) > 4.0
 );
+
+
+SELECT u.user_id, u.email
+FROM User_Table u
+WHERE (
+    SELECT COUNT(*)
+    FROM booking b
+    JOIN Profile_Table p ON b.booked_by = p.profile_id
+    WHERE p.user_id = u.user_id
+) > 3;
