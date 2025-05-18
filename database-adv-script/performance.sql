2c3c7a4d7288bc5b02c2aa41b2b162c06ab63fe8
samueldanalyst <samuelvictor747futa@gmail.com>
2025-05-18 14:50:03 +0100

    optimize

--- database-adv-script/performance.sql
+++ database-adv-script/performance.sql
@@ -1,61 +1,20 @@
 SELECT 
     b.booking_id,
-    b.created_at AS booking_date,
     b.startdate,
     b.enddate,
-    b.status,
+    b.status AS booking_status,
 
-    -- User details
+    -- Property details
+    p.property_id,
+    p.title AS property_title,
+    p.description,
+    p.location AS property_location,
+    p.rent,
+
+    -- User details of the person who booked
     u.user_id,
     u.email,
     u.account_status,
-
-    -- Profile details
-    p.first_name,
-    p.last_name,
-    p.phone_number,
-
-    -- Property details
-    pl.property_id,
-    pl.title AS property_title,
-    pl.description,
-    pl.rent,
-
-    -- Payment details
-    pay.payment_id,
-    pay.payment_amount,
-    pay.payment_status
-
-FROM bookings b
-JOIN profiles p ON b.profile_id = p.profile_id
-JOIN users u ON p.user_id = u.user_id
-JOIN properties pl ON b.property_id = pl.property_id
-JOIN payment pay ON b.payment_id = pay.payment_id;
-
-
-EXPLAIN ANALYZE
-SELECT 
-    b.booking_id,
-    b.created_at AS booking_date,
-    b.startdate,
-    b.enddate,
-    b.status,
-
-    -- User details
-    u.user_id,
-    u.email,
-    u.account_status,
-
-    -- Profile details
-    p.first_name,
-    p.last_name,
-    p.phone_number,
-
-    -- Property details
-    pl.property_id,
-    pl.title AS property_title,
-    pl.description,
-    pl.rent,
 
     -- Payment details
     pay.payment_id,
@@ -63,7 +22,35 @@ SELECT
     pay.payment_status
 
 FROM booking b
-JOIN profile_table p ON b.profile_id = p.profile_id
-JOIN user_table u ON p.user_id = u.user_id
-JOIN property_listing pl ON b.property_id = pl.property_id
-JOIN payment pay ON b.payment_id = pay.payment_id;
+JOIN property_listing p ON b.property_id = p.property_id
+JOIN Profile_Table prof ON b.booked_by = prof.profile_id
+JOIN User_Table u ON prof.user_id = u.user_id
+LEFT JOIN payment pay ON b.payment_id = pay.payment_id;
+
+
+
+Analyze the query’s performance using EXPLAIN and identify any inefficiencies
+
+
+
+SELECT 
+    b.booking_id,
+    b.startdate,
+    b.enddate,
+    b.status AS booking_status,
+    p.property_id,
+    p.title AS property_title,
+    p.description,
+    p.location AS property_location,
+    p.rent,
+    u.user_id,
+    u.email,
+    u.account_status,
+    pay.payment_id,
+    pay.payment_amount,
+    pay.payment_status
+FROM booking b
+JOIN property_listing p ON b.property_id = p.property_id
+JOIN Profile_Table prof ON b.booked_by = prof.profile_id
+JOIN User_Table u ON prof.user_id = u.user_id
+LEFT JOIN payment pay ON b.payment_id = pay.payment_id;
