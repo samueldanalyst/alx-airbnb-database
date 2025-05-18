f61fb5e9d9d58415301a593d6cc9f142172d52d3
samueldanalyst <samuelvictor747futa@gmail.com>
2025-05-18 13:25:55 +0100

    push windows function

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -20,3 +20,22 @@ LEFT JOIN
     bookings b ON p.id = b.property_id
 GROUP BY 
     p.id, p.title;
+
+
+SELECT 
+    property_id,
+    title,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS rank_number
+FROM (
+    SELECT 
+        p.id AS property_id,
+        p.title,
+        COUNT(b.id) AS total_bookings
+    FROM 
+        properties p
+    LEFT JOIN 
+        bookings b ON p.id = b.property_id
+    GROUP BY 
+        p.id, p.title
+) AS property_bookings;
